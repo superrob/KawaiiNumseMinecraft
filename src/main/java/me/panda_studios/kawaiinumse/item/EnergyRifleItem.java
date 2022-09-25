02e61d60c7da2f1d76c8f159101c1060ce5a4464
PandaDap2006 <Oliver.h.froberg@gmail.com>
2022-09-25 21:25:23 +0200

    Fixed some mistakes with the Energy rifle

--- src/main/java/me/panda_studios/kawaiinumse/item/EnergyRifleItem.java
+++ src/main/java/me/panda_studios/kawaiinumse/item/EnergyRifleItem.java
@@ -25,18 +25,38 @@ public class EnergyRifleItem extends Item {
     }
     @Override
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand interactionHand) {
+        ItemStack ammoItemStack = new ItemStack(ItemSetup.RAINBOW_ENERGY.get());
+        ItemStack itemStack = player.getItemInHand(interactionHand);
+        ItemStack ammo = null;
+        for (int i = 0; i < player.getInventory().getContainerSize(); i++) {
+            if (player.getInventory().getItem(i).sameItem(ammoItemStack)) {
+                ammo = player.getInventory().getItem(i);
+                break;
+            }
+        }
+        if (ammo == null && !player.isCreative()) {
+            return InteractionResultHolder.fail(itemStack);
+        }
+
         level.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.EGG_THROW, SoundSource.PLAYERS, 0.5F, 0.4F / (level.getRandom().nextFloat() * 0.4F + 0.8F));
-        player.getCooldowns().addCooldown(this, 2);
+        player.getCooldowns().addCooldown(this, 1);
         if (!level.isClientSide) {
             ThrownEnergyball thrownEnergyball = new ThrownEnergyball(level, player);
             thrownEnergyball.setItem(new ItemStack(ItemSetup.RAINBOW_ENERGY.get()));
             thrownEnergyball.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, 1.5F, 1.0F);
             level.addFreshEntity(thrownEnergyball);
         }
-        player.getItemInHand(interactionHand).hurtAndBreak(1, player, (event) -> {
+        itemStack.hurtAndBreak(1, player, (event) -> {
             event.broadcastBreakEvent(player.getUsedItemHand());
         });
 
-        return InteractionResultHolder.sidedSuccess(player.getItemInHand(interactionHand), level.isClientSide());
+        if (!player.isCreative()) {
+            ammo.shrink(1);
+            if (ammo.isEmpty()) {
+                player.getInventory().removeItem(ammo);
+            }
+        }
+
+        return InteractionResultHolder.sidedSuccess(itemStack, level.isClientSide());
     }
 }
